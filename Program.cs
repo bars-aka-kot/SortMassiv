e7de662783715bc240ca01bbb1669f6d042a03ab
Basirov Ilgiz <Basirov_ilgiz@mail.ru>
2024-04-14 21:29:50 +0500

    Change code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class SortMassiv
+{
+    static void Main(string[] args)
+    {
+        int[,] massiv = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+
+        int k = massiv.GetLength(0);
+        int l = massiv.GetLength(1);
+
+        int[] array = new int[k*l];
+
+        int z = 0;
+        for (int i = 0; i < l; i++)
+        {   
+            for (int j = 0; j < k; j++)
+            {
+                array[z] = massiv[i,j];
+                z++;
+            }
+        }
+
+        Array.Sort(array);
+        z = 0;
+
+        for (int i = 0; i < l; i++)
+        {
+            for(int j=0;j< k; j++)
+            {
+                massiv[i,j] = array[z];
+                z++;
+                Console.Write(massiv[i,j] + " ");
+            }
+           Console.WriteLine();
+        }        
+    }
+}
