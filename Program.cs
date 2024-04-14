4badea88b62030bcdf734933f5e46e118604655f
Basirov Ilgiz <Basirov_ilgiz@mail.ru>
2024-04-14 21:47:46 +0500

    updated code

--- Program.cs
+++ Program.cs
@@ -3,34 +3,54 @@
     static void Main(string[] args)
     {
         int[,] massiv = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
-
-        int k = massiv.GetLength(0);
-        int l = massiv.GetLength(1);
-
-        int[] array = new int[k*l];
-
+        int[] array = new int[massiv.GetLength(0) * massiv.GetLength(1)];
         int z = 0;
-        for (int i = 0; i < l; i++)
-        {   
-            for (int j = 0; j < k; j++)
+
+        PrintMatrix(massiv, "Первоначальная матрица");
+        for (int i = 0; i < massiv.GetLength(1); i++)
+        {
+            for (int j = 0; j < massiv.GetLength(0); j++)
             {
-                array[z] = massiv[i,j];
+                array[z] = massiv[i, j];
                 z++;
             }
         }
-
+        PrintArray(array,"Несортированный одномерный массив");
         Array.Sort(array);
+        PrintArray(array, "Сортированный одномерный массив");
+
         z = 0;
 
-        for (int i = 0; i < l; i++)
+        for (int i = 0; i < massiv.GetLength(1); i++)
         {
-            for(int j=0;j< k; j++)
+            for (int j = 0; j < massiv.GetLength(0); j++)
             {
-                massiv[i,j] = array[z];
+                massiv[i, j] = array[z];
                 z++;
-                Console.Write(massiv[i,j] + " ");
             }
-           Console.WriteLine();
-        }        
+        }
+        PrintMatrix(massiv, "Сортированная матрица");
+    }
+    public static void PrintArray(int[] arr,string text)
+    {
+        Console.WriteLine($"{text}");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public static void PrintMatrix(int[,] matrix, string text)
+    {
+        Console.WriteLine($"{text}");
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
